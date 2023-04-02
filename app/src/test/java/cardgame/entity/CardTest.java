26763df051c6eb920827f5c78ca0e9f234b5fb43
Vaibhav-RP <vrp8698231262@gmail.com>
2023-04-02 12:27:28 +0530

    card test

---
+++ app/src/test/java/cardgame/entity/CardTest.java
@@ -0,0 +1,28 @@
+package cardgame.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import cardgame.enums.Rank;
+import cardgame.enums.Suit;
+
+@DisplayName(" Card Class Test ")
+public class CardTest {
+    
+    @Test
+    @DisplayName(" Card Class constructor test")
+    public void testConstructor() {
+        Card card = new Card(Rank.ACE, Suit.SPADES);
+        assertEquals(Rank.ACE, card.getRank());
+        assertEquals(Suit.SPADES, card.getSuit());
+    }
+
+    @Test
+    @DisplayName(" Card class toString() result test ")
+    public void testToString() {
+        Card card = new Card(Rank.KING, Suit.HEARTS);
+        assertEquals("KING of HEARTS", card.toString());
+    }
+}
